2dd206ef385c2a4c5b3b7ac79099b6a86011e44e
Diego Cruz Aguilar <dcrag@pm.me>
2022-12-12 13:27:21 -0600

    better config

--- config/nvim/after/ftplugin/cs.lua
+++ config/nvim/after/ftplugin/cs.lua
@@ -2,26 +2,27 @@
 --   name = "csharp_ls",
 --   cmd = { "csharp-ls" },
 --   -- root_dir = vim.fs.dirname(vim.fs.find({'Prueba.sln', 'Prueba.csproj'}, { upward = true })[1]),
---   root_dir = require 'lspconfig'.util.root_pattern('*.csproj')(vim.loop.cwd()),
+--   root_dir = require("lspconfig").util.root_pattern "*.csproj"(vim.loop.cwd()),
 --   init_options = { AutomaticWorkspaceInit = true },
 --   handlers = {
---     ["textDocument/definition"] = require('csharpls_extended').handler,
---   }
+--     ["textDocument/definition"] = require("csharpls_extended").handler,
+--   },
 -- }
+-- require("config.lsp").setup(csharp_ls)
 
 local omnisharp = {
   handlers = {
-    ['textDocument/definition'] = require('omnisharp_extended').handler
+    ["textDocument/definition"] = require("omnisharp_extended").handler,
   },
   -- cmd = { "dotnet", "/usr/lib/omnisharp-roslyn/OmniSharp.dll" },
-  cmd = { '/usr/bin/omnisharp', '--languageserver', '--hostPID', tostring(pid) },
+  cmd = { "/usr/bin/omnisharp", "--languageserver", "--hostPID", tostring(pid) },
   enable_editorconfig_support = true,
   enable_ms_build_load_projects_on_demand = false,
   enable_roslyn_analyzers = true,
   organize_imports_on_format = true,
   enable_import_completion = true,
-  sdk_include_prereleases = true,
-  analyze_open_documents_only = false,
-  root_dir = require 'lspconfig'.util.root_pattern('*.csproj')(vim.loop.cwd()),
+  sdk_include_prereleases = false,
+  analyze_open_documents_only = true,
+  root_dir = require("lspconfig").util.root_pattern "*.csproj"(vim.loop.cwd()),
 }
 require("config.lsp").setup(omnisharp)
